c88564a4a73218488f94f12f2f663c7279811b2d
rvitalia <riabinines@yandex.ru>
2023-02-28 00:00:14 +0500

    form

---
+++ src/form.php
@@ -0,0 +1,8 @@
+<form action="" method="post">
+    <label for="" class="">
+        <input type="text">
+    </label>
+    <label for="" class="">
+        <input type="password">
+    </label>
+</form>
